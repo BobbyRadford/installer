00bac3485570540a1b6f1e2987d179f80f18de0d
Bobby Radford <bobby.radford@ibm.com>
2021-04-28 15:14:52 -0400

    ibmcloud: add fields to Platform type
    Add the DefaultMachinePlatform, VPC, VPCResourceGroup, and Subnets
    fields to the Platform type. These are needed to fully define a
    cluster.

--- pkg/types/ibmcloud/platform.go
+++ pkg/types/ibmcloud/platform.go
@@ -2,11 +2,6 @@ package ibmcloud
 
 // Platform stores all the global configuration that all machinesets use.
 type Platform struct {
-	// ResourceGroup is the name of an existing resource group where the cluster
-	// and all required resources will be created.
-	// +optional
-	ResourceGroup string `json:"resourceGroup,omitempty"`
-
 	// Region specifies the IBM Cloud region where the cluster will be
 	// created.
 	Region string `json:"region"`
@@ -17,6 +12,30 @@ type Platform struct {
 
 	// ClusterOSImage is the name of the custom RHCOS image.
 	ClusterOSImage string `json:"clusterOSImage"`
+
+	// ResourceGroup is the name of an existing resource group where the cluster
+	// and all required resources will be created.
+	// +optional
+	ResourceGroup string `json:"resourceGroup,omitempty"`
+
+	// DefaultMachinePlatform is the default configuration used when installing
+	// on IBM Cloud for machine pools which do not define their own platform
+	// configuration.
+	// +optional
+	DefaultMachinePlatform *MachinePool `json:"defaultMachinePlatform,omitempty"`
+
+	// VPC is the name of an existing VPC network.
+	// +optional
+	VPC string `json:"vpc,omitempty"`
+
+	// VPCResourceGroup is he name of the existing VPC's resource group.
+	// +optional
+	VPCResourceGroup string `json:"vpcResourceGroup,omitempty"`
+
+	// Subnets is a list of existing subnet IDs. Leave unset and the installer
+	// will create new subnets in the VPC network on your behalf.
+	// +optional
+	Subnets []string `json:"subnets,omitempty"`
 }
 
 // SetBaseDomain sets the CISInstanceCRN.
